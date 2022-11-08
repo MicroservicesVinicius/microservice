00c3168d51069844a977f506db6c93462d8daf97
João Paulo <jpsilva.math@icloud.com>
2022-11-08 15:29:35 -0400

    Bháskara
    Signed-off-by: João Paulo <jpsilva.math@icloud.com>

---
+++ src/main/java/finalProjetoMicroservice/tads/micro/controller/ConsolmeMult.java
@@ -0,0 +1,13 @@
+package finalProjetoMicroservice.tads.micro.controller;
+
+public class ConsolmeMult {
+
+
+    public double delta(int a, int b , int c){
+        return Math.pow(b, 2)-(4*a*c);
+    }
+
+
+
+    
+}
